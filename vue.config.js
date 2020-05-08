181ca71da335bca576b6b7e921ec870d5393a3b7
NIBAOBAOZHALE <624940523@qq.com>
2020-05-08 23:11:12 +0800

    资源路径加上引号

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   lintOnSave: false,
   publicPath: './',
+
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].minify = false
       return args
     })
-  }
+  },
+
+  productionSourceMap: false
 }
