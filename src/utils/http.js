4ceb3b5b414560a794025c532b3af97007163718
fanteality <1454001183@qq.com>
2020-05-10 22:28:30 +0800

    重新封装ajax请求第一步

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,10 +1,41 @@
 class Http {
-  constructor() {
-    this.baseUrl = 'http://localhost:3001'
+  constructor(
+    {
+      url = '',
+      method = 'get',
+      data = {},
+      async = true,
+      success,
+      error,
+      progress,
+      headers = {}
+    },
+    time = 5000
+  ) {
     this.baseUrl = process.env.VUE_APP_BASE_URL
     this.xmlhttp = new XMLHttpRequest()
-    this.progress = 0
+    this.option = { url, data, success, error, progress, headers, time }
   }
+  start() {
+    this.checkOption()
+  }
+  // 配置校验
+  checkOption() {
+    let { url, header, time, data } = this.option
+    if (typeof url !== 'string' || url === '') {
+      throw new Error('url必须是不为空的字符串')
+    }
+    if (typeof header !== 'object') {
+      throw new Error('请求头必须以键值对的形式传入')
+    }
+    if (typeof data !== 'object') {
+      throw new Error('参数必须以键值对的形式传入')
+    }
+    if (typeof time !== 'number') {
+      throw new Error('超时时间必须为number类型')
+    }
+  }
+
   //   执行ajax
   runAjax(methods, api, params) {
     return new Promise((resolve, reject) => {
@@ -68,10 +99,6 @@ class Http {
       this.progress = e.loaded
       console.log(e.loaded)
     })
-    // this.xmlhttp.upload.onprogress = function(e) {
-    //   this.progress = e.loaded
-    //   console.log(e.loaded)
-    // }
   }
 }
 export default new Http()
