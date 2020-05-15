686753c5bba08ad672aa3be513d866fb2f1d8266
NIBAOBAOZHALE <why624940523@gmail.com>
2020-05-15 15:33:30 +0800

    更改 打包模式为开发模式
    Signed-off-by: NIBAOBAOZHALE <why624940523@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   lintOnSave: false,
   publicPath: './',
-
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].minify = false
       return args
     })
   },
-
   productionSourceMap: false
 }
